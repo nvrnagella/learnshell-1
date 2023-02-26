d36fcfffb1251c8ceacfbee1801f1da171a50909
IT1089778 <nvrnagella@gmail.com>
2023-02-26 08:02:42 +0530

    coloring your text

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,10 @@
 echo -e "\n"
 echo Hello world
 echo "**** Hellow world ****"
+
+echo -e "\e[31m MESSAGE \e[0m"
+echo -e "\e[32m MESSAGE \e[0m"
+echo -e "\e[33m MESSAGE \e[0m"
+echo -e "\e[34m MESSAGE \e[0m"
+echo -e "\e[35m MESSAGE \e[0m"
+echo -e "\e[36m MESSAGE \e[0m"
