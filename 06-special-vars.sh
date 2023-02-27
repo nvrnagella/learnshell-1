63276b600f2e4e1b182bad2cc833113c74825033
IT1089778 <nvrnagella@gmail.com>
2023-02-27 23:14:29 +0530

    sending special variables inside function

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -5,3 +5,15 @@ echo "3 - $3"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+example (){
+  echo i am reporting from inside function
+  echo "0 - $0"
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "3 - $3"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
+
+example
