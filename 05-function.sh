22b6ec28811a951fb2c1dbe67560add4593c29f2
IT1089778 <nvrnagella@gmail.com>
2023-02-27 21:21:08 +0530

    declared function and calling function

---
+++ 05-function.sh
@@ -0,0 +1,9 @@
+#function is like a variables
+#variables - if we assign a name to set of data then its a variable
+#function - if we assign a name to set of commands then it is called a function
+#how to declare a function
+example (){
+  echo i am reporting from inside function
+}
+
+example
